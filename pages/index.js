a7c448f3deefa94c18e73b9187715e1db4db6e73
Usama12-sketch <chodarykhan115@gmail.com>
2023-02-22 08:41:35 -0700

    Update index

--- pages/index.js
+++ pages/index.js
@@ -250,25 +250,9 @@ export default function Home() {
             <div className="w-full md:w-6/12 px-4 mr-auto ml-auto mt-32">
               <div className="justify-center flex flex-wrap relative">
                 <div className="my-4 w-full lg:w-6/12 px-4">
-                  <a
-                    href="https://www.creative-tim.com/learning-lab/tailwind/svelte/alerts/notus?ref=vtw-index"
-                    target="_blank"
-                  >
-                    <div className="bg-red-600 shadow-lg rounded-lg text-center p-8">
-                      <img
-                        alt="..."
-                        className="shadow-md rounded-full max-w-full w-16 mx-auto p-2 bg-white"
-                        src="https://raw.githubusercontent.com/creativetimofficial/public-assets/master/logos/svelte.jpg"
-                      />
-                      <p className="text-lg text-white mt-4 font-semibold">
-                        Svelte
-                      </p>
-                    </div>
-                  </a>
-                  <a
-                    href="https://www.creative-tim.com/learning-lab/tailwind/react/alerts/notus?ref=vtw-index"
-                    target="_blank"
-                  >
+                  
+              
+                
                     <div className="bg-lightBlue-500 shadow-lg rounded-lg text-center p-8 mt-8">
                       <img
                         alt="..."
@@ -279,11 +263,8 @@ export default function Home() {
                         ReactJS
                       </p>
                     </div>
-                  </a>
-                  <a
-                    href=""
-                    target="_blank"
-                  >
+                  
+             
                     <div className="bg-blueGray-700 shadow-lg rounded-lg text-center p-8 mt-8">
                       <img
                         alt="..."
@@ -294,13 +275,10 @@ export default function Home() {
                         NextJS
                       </p>
                     </div>
-                  </a>
+                
                 </div>
                 <div className="my-4 w-full lg:w-6/12 px-4 lg:mt-16">
-                  <a
-                    href="#"
-                    target="_blank"
-                  >
+                 
                     <div className="bg-yellow-500 shadow-lg rounded-lg text-center p-8">
                       <img
                         alt="..."
@@ -311,7 +289,7 @@ export default function Home() {
                         JavaScript
                       </p>
                     </div>
-                  </a>
+              
                  
                   
                 </div>
